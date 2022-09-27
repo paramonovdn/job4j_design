10b0f8d3e5c121907562e3a9297a8f195a9c3611
Paramonov Dmitriy <paramonovdn@mail.ru>
2022-09-27 22:08:43 +0300

    5.1.2.Итератор четных чисел [#150] - 6

--- src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
+++ src/main/java/ru/job4j/iterator/EvenNumbersIterator.java
@@ -11,7 +11,7 @@ public class EvenNumbersIterator implements Iterator<Integer> {
     }
     @Override
     public boolean hasNext() {
-       while (index < data.length && !(data[index] % 2 == 0)) {
+       while (index < data.length && data[index] % 2 != 0) {
            index++;
        }
        return index < data.length;
