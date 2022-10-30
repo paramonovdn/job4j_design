f2426be28f3c14bb9aab835ad3606b53539047db
Paramonov Dmitriy <paramonovdn@mail.ru>
2022-10-30 21:02:35 +0300

    6. Перевернуть связанный список [#161]

--- src/test/java/ru/job4j/collection/ForwardLinkedTest.java
+++ src/test/java/ru/job4j/collection/ForwardLinkedTest.java
@@ -1,46 +1,37 @@
 package ru.job4j.collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.util.Iterator;
+import org.junit.jupiter.api.BeforeEach;
 import java.util.NoSuchElementException;
 
 class ForwardLinkedTest {
     private ForwardLinked<Integer> linked;
 
     @BeforeEach
-    public void init() {
+    void init() {
         linked = new ForwardLinked<>();
+    }
+
+    @Test
+    void whenSize0ThenReturnFalse() {
+        assertThat(linked.revert()).isFalse();
+    }
+
+    @Test
+    void whenSize1ThenReturnFalse() {
+        linked.add(1);
+        assertThat(linked.revert()).isFalse();
+    }
+
+    @Test
+    void whenAddAndRevertTrue() {
         linked.add(1);
         linked.add(2);
         linked.add(3);
         linked.add(4);
+        assertThat(linked).containsSequence(1, 2, 3, 4);
+        assertThat(linked.revert()).isTrue();
+        assertThat(linked).containsSequence(4, 3, 2, 1);
     }
-
-    @Test
-    void whenDeleteFirst() {
-        assertThat(linked.deleteFirst()).isEqualTo(1);
-        assertThat(linked.deleteFirst()).isEqualTo(2);
-        assertThat(linked.deleteFirst()).isEqualTo(3);
-        assertThat(linked.deleteFirst()).isEqualTo(4);
-        assertThatThrownBy(linked.iterator()::next)
-                .isInstanceOf(NoSuchElementException.class);
-    }
-
-    @Test
-    void whenDeleteEmptyLinked() {
-        ForwardLinked<Integer> linked = new ForwardLinked<>();
-        assertThatThrownBy(linked::deleteFirst)
-                .isInstanceOf(NoSuchElementException.class);
-    }
-    @Test
-    void whenMultiDelete() {
-        linked.deleteFirst();
-        Iterator<Integer> it = linked.iterator();
-        assertThat(it.next()).isEqualTo(2);
-    }
-
 }
