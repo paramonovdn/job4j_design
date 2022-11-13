2ae80ef83a36ffa57b35023724f5149877c0ae1a
Paramonov Dmitriy <paramonovdn@mail.ru>
2022-11-13 16:46:23 +0300

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/collection/User.java
+++ src/main/java/ru/job4j/collection/User.java
@@ -1,6 +1,6 @@
 package ru.job4j.collection;
 
-import java.util.Calendar;
+import java.util.*;
 
 public class User {
     private String name;
@@ -11,5 +11,29 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        User user1 = new User("Stivie", 1, new GregorianCalendar(1990, 10, 20));
+        User user2 = new User("Stivie", 1, new GregorianCalendar(1990, 10, 20));
+        Map<User, Object> map = new HashMap<>(16);
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+        String i = "";
+        Iterator<Map.Entry<User, Object>> it = map.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry<User, Object> pair = it.next();
+            System.out.println(pair.getKey() + ", " + pair.getValue());
+        }
+        int hashCode1 = user1.hashCode();
+        int hash1 = hashCode1 ^ (hashCode1 >>> 16);
+        int backet1 = hash1 & 15;
+        int hashCode2 = user2.hashCode();
+        int hash2 = hashCode2 ^ (hashCode2 >>> 16);
+        int backet2 = hash2 & 15;
+        System.out.println();
+        System.out.printf("user1 - хешкод: %s, хеш: %s, бакет: %s", hashCode1, hash1, backet1);
+        System.out.println();
+        System.out.printf("user2 - хешкод: %s, хеш: %s, бакет: %s", hashCode2, hash2, backet2);
     }
 }
