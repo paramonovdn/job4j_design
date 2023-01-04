ba1108146d1139c9d6fcd903e79813cec552e908
Paramonov Dmitriy <paramonovdn@mail.ru>
2023-01-04 19:28:21 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -10,8 +10,8 @@ public class ResultFile {
         String firstLine = "* |1 2 3 4 5 6 7 8 9 10" + "\n";
         String limiter = "_________________________" + "\n";
 
-        for (int i = 1; i <= 10; i++){
-            for (int j = 1; j <= 10; j++){
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
                 result = i * j;
                 s += String.valueOf(result) + " ";
             }
