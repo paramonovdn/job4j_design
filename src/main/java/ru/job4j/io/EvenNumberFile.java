39c7320531478253c1b8209d5ca6e7323d37df72
Paramonov Dmitriy <paramonovdn@mail.ru>
2023-01-04 19:49:40 +0300

    0.2. FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,27 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        StringBuilder text = null;
+        try (FileInputStream in = new FileInputStream("even.txt")) {
+            text = new StringBuilder();
+            int read;
+            while ((read = in.read()) != -1) {
+                text.append((char) read);
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                boolean rsl = Integer.parseInt(line) % 2 == 0;
+                if (rsl) {
+                    System.out.println(line + " -четное число");
+                } else  {
+                    System.out.println(line + " -нечетное число");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
