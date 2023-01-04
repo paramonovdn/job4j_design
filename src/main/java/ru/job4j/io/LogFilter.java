020c796fca2959f254e88f861987d68c934ba43c
Paramonov Dmitriy <paramonovdn@mail.ru>
2023-01-04 20:37:18 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,29 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LogFilter {
+    public List<String> filter(String file) {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            for (String line = in.readLine(); line != null; line = in.readLine()) {
+                if (line.contains(" 404 ")) {
+                    list.add(line);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter();
+        List<String> log = logFilter.filter("log.txt");
+        System.out.println(log);
+    }
+}
