0deb5cc3e74aefd176f0c9777c43f62f555c978c
Paramonov Dmitriy <paramonovdn@mail.ru>
2023-02-04 20:17:18 +0300

    11. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -1,30 +1,25 @@
 package ru.job4j.io.duplicates;
-import ru.job4j.io.Dir;
 
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    Map<FileProperty, LinkedList<Path>> duplicates = new HashMap<>();
+    private Map<FileProperty, List<Path>> duplicates = new HashMap<>();
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         String pathName = file.getFileName().toString();
         Long pathSize = attrs.size();
         Path path = file.toAbsolutePath();
         FileProperty fileProperty = new FileProperty(pathSize, pathName);
-        if (duplicates.containsKey(fileProperty)) {
-            LinkedList<Path> pathes = duplicates.get(fileProperty);
-            pathes.add(path);
-            duplicates.put(fileProperty, pathes);
-        } else {
-            LinkedList<Path> pathes = new LinkedList<>();
+        List<Path> pathes = new LinkedList<>();
+        pathes.add(path);
+        List<Path> value = duplicates.putIfAbsent(fileProperty, pathes);
+        if (value != null){
+            pathes = duplicates.get(fileProperty);
             pathes.add(path);
             duplicates.put(fileProperty, pathes);
         }
@@ -32,9 +27,9 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     }
 
     public void printDuplicateMap() {
-        for (Map.Entry<FileProperty, LinkedList<Path>> pair : duplicates.entrySet()) {
+        for (Map.Entry<FileProperty, List<Path>> pair : duplicates.entrySet()) {
             FileProperty key = pair.getKey();
-            LinkedList<Path> value = pair.getValue();
+            List<Path> value = pair.getValue();
             if (value.size() > 1) {
                 System.out.println(key.getName() + "- " + key.getSize() + " байт");
                 for (Path path : value) {
