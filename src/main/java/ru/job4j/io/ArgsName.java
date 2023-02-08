a45fdc2cf6e4bc7935e57d6e961e4ef12e50526a
Paramonov Dmitriy <paramonovdn@mail.ru>
2023-02-08 15:36:19 +0300

    13. Именованные аргументы [#295518 #388531]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -34,30 +34,28 @@ public class ArgsName {
     }
 
     private static void argValidation(String arg, int symbolPosition) {
-            if (!arg.contains("=")) {
-                throw new IllegalArgumentException(String.format("In pair missing separator \"=\"."
-                        + " Invalid pair: %s.", arg));
-            } else {
-                String key = arg.substring(0, symbolPosition);
-                String value = arg.substring(symbolPosition + 1, arg.length());
-                if (!key.startsWith("-")) {
-                    throw new IllegalArgumentException(String.format("The key must start with \"-\"."
-                            + " Invalid pair: %s", arg));
-                }
-                key = arg.substring(1, symbolPosition);
-                if (key.isEmpty() || value.isEmpty()) {
-                    throw new IllegalArgumentException(String.format("Key or values in pair is empty."
-                            + " Invalid pair: %s", arg));
-                }
-            }
-
+        if (!arg.contains("=")) {
+            throw new IllegalArgumentException(String.format("In pair missing separator \"=\"."
+                    + " Invalid pair: %s.", arg));
+        }
+        String key = arg.substring(0, symbolPosition);
+        String value = arg.substring(symbolPosition + 1, arg.length());
+        if (!key.startsWith("-")) {
+            throw new IllegalArgumentException(String.format("The key must start with \"-\"."
+                    + " Invalid pair: %s", arg));
+        }
+        key = arg.substring(1, symbolPosition);
+        if (key.isEmpty() || value.isEmpty()) {
+            throw new IllegalArgumentException(String.format("Key or values in pair is empty."
+                    + " Invalid pair: %s", arg));
+        }
     }
 
     public static void main(String[] args) {
         ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
-        System.out.println(jvm.get("Xmx"));
+       // System.out.println(jvm.get("Xmx"));
 
         ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
-        System.out.println(zip.get("out"));
+       // System.out.println(zip.get("out"));
     }
 }
