9a2ca23c01fdd2b0945d721c3860f488b04736b6
Paramonov Dmitriy <paramonovdn@mail.ru>
2023-02-13 17:36:27 +0300

    14. Архивировать проект [#861 #389653]

--- src/main/java/ru/job4j/io/output/Zip.java
+++ src/main/java/ru/job4j/io/output/Zip.java
@@ -16,11 +16,11 @@ import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
-    public void packFiles(List<File> sources, File target) {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (File source : sources) {
-                zip.putNextEntry(new ZipEntry(source.getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source.toFile().getPath()))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -41,19 +41,33 @@ public class Zip {
         }
     }
 
+    private static void argValidation(String directory, String exclude, String output) {
+        if (!Files.isDirectory(Paths.get(directory))) {
+            throw new IllegalArgumentException(String.format("The directory does not exist: %s", directory));
+        }
+        if (!exclude.startsWith(".") || exclude.length() < 2) {
+            throw new IllegalArgumentException(String.format("The extension of the exception files is not defined: %s",
+                    exclude));
+        }
+        int pointPosition = output.indexOf(".");
+        String fileName = output.substring(1, pointPosition);
+        if (fileName.length() < 1) {
+            throw new IllegalArgumentException(String.format("The file name is incorrect: %s",
+                    output));
+        }
+        if (!output.endsWith(".zip")) {
+            throw new IllegalArgumentException(String.format("The extension of the final archive must be- *.zip: %s",
+                    output));
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         ArgsName argsName = ArgsName.of(args);
         String directory = argsName.get("d");
         String exclude = argsName.get("e");
         String output = argsName.get("o");
-        if (!Files.isDirectory(Paths.get(directory))) {
-            throw new IllegalArgumentException(String.format("The directory does not exist: %s", directory));
-        }
-        List<Path> excludeFiles =  Search.search(Paths.get(directory), p -> p.toFile().getName().endsWith(exclude));
-        List<Path> allFiles =  Files.walk(Paths.get(directory)).filter(Files::isRegularFile).collect(Collectors.toList());
-        allFiles.removeAll(excludeFiles);
-        List<File> filesForArchiving = allFiles.stream().map(f -> f.toFile()).collect(Collectors.toList());
-
+        argValidation(directory, exclude, output);
+        List<Path> filesForArchiving =  Search.search(Paths.get(directory), p -> !p.toFile().getName().endsWith(exclude));
         String myDirectory = "C:\\backups\\" + output;
         Zip zip = new Zip();
         zip.packFiles(filesForArchiving, new File(myDirectory));
