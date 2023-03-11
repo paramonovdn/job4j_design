262ae0a7bf54129b739fb7e8bbf6800ce1927980
Paramonov Dmitriy <paramonovdn@mail.ru>
2023-03-11 17:57:02 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,15 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+       byte anyByte = 127;
+       short anyShort = 32767;
+       int anyInt = 2147483647;
+       long anyLong = 654654654l;
+       float anyFloat = 5465468746868468f;
+       double anyDooble = 6546546.54d;
+       char anyChar = 'a';
+       boolean anyBoolean = true;
+       LOG.debug("We have 8 examples of primitive variables: byte: {}, short: {}, int: {}, long: {}, float: {}, double: {}," +
+               " char: {}, boolean: {}.", anyByte, anyShort, anyInt, anyLong, anyFloat, anyDooble, anyChar, anyBoolean);
     }
 }
