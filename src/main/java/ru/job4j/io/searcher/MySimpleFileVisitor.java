2a24c17a4c69770fa2838b75e5d0672321fb6dde
Paramonov Dmitriy <paramonovdn@mail.ru>
2023-04-20 11:45:58 +0300

    3. Поиск файлов по критерию [#783] - 3

--- src/main/java/ru/job4j/io/searcher/MySimpleFileVisitor.java
+++ src/main/java/ru/job4j/io/searcher/MySimpleFileVisitor.java
@@ -5,12 +5,24 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
 public class MySimpleFileVisitor extends SimpleFileVisitor<Path> {
     public static List<String> foundFiles = new ArrayList<>();
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         if ("mask".equals(Searcher.getSearchType())) {
-            if (file.getFileName().toString().matches("." + Searcher.getSearchFileName())) {
+            String mask = Searcher.getSearchFileName().replace(".", "\\.")
+                    .replace("?", ".?")
+                    .replace("*", ".+");
+            Pattern pattern = Pattern.compile(mask);
+            Predicate<Path> predicate = p -> {
+                String fileName = p.toFile().getName();
+                return pattern.matcher(fileName).find();
+            };
+            if (predicate.test(file)) {
                 foundFiles.add(file.toAbsolutePath().toString());
             }
         }
